046b0221ec721367ca53b5d0edc6d6af03655aeb
Nipun Khanna <nipunkhanna821@gmail.com>
2025-01-16 20:08:50 -0500

    Update data and add full preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,20 @@ import pandas
 
 df = pandas.read_csv("data/raw_data.csv")
 
-for index, row in df.iterrows():
-    column = row["Text"]
+df = df.rename(columns={"Datetime":"Date"})
+df.drop("Unnamed: 0", axis=1, inplace=True)
+
+excluded_chars = ["\n", "*", ">", "#"]
+for replace in excluded_chars:
+    df["Text"] = df["Text"].str.replace(replace, "")
+
+
+def cleanDate(original):
+    return str(original).split()[0]
+
+df["Date"] = df["Date"].apply(cleanDate)
+
+
+df.to_csv("data/preprocessed_data.csv", index=False)
+
     
